235d28ff311014e143fbd309ac515c06b4492502
Toli Leonovich <schmoli@gmail.com>
2020-11-09 06:50:05 -0800

    fixing e2e tests in template

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('schmoli-root .content span')).getText() as Promise<string>;
+    return element(by.css('schmoli-root')).getText() as Promise<string>;
   }
 }
